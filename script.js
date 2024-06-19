c4980e17a86c6a8d16573009b017206b92cdc16f
hoang <haong@gmail.com>
2024-06-19 18:54:11 +0800

    hello

---
+++ script.js
@@ -0,0 +1,31 @@
+function getMail() {
+    // Get the text field
+    var copyText = document.getElementById("mail");
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+}
+
+function getHotline() {
+    // Get the text field
+    var copyText = document.getElementById("hotline");
+
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+}
+
+
