2fd867efb1b49e8f0a0c7bf845f64b3057611b03
hoangdeptrai <haong@gmail.com>
2024-06-30 08:51:12 +0800

    update my portfolio

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
-function getMail() {
-    // Get the text field
-    var copyText = document.getElementById("mail");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-
-    // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
-}
-
-function getHotline() {
-    // Get the text field
-    var copyText = document.getElementById("hotline");
-
-    // Select the text field
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); // For mobile devices
-
-    // Copy the text inside the text field
-    navigator.clipboard.writeText(copyText.value);
-
-    // Alert the copied text
-    alert("Copied the text: " + copyText.value);
-}
-
-
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
